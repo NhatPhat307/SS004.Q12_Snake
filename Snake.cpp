15f14d232d1a139eb29da08af18db10a8c1800c0
NhatPhat307 <nhatphat30706@gmail.com>
2025-11-16 23:22:11 +0700

    Refactor Snake class structure and improve code readability

--- Snake.cpp
+++ Snake.cpp
@@ -2,69 +2,49 @@
 #include <windows.h>
 #include <cstdlib>
 #include <conio.h>
+#include <ctime>
 using namespace std;
-void gotoxy( int column, int line );
-struct Point{
-    int x,y;
+
+void gotoxy(int column, int line);
+
+// Kích thước khung chơi
+const int LEFT   = 0;
+const int TOP    = 0;
+const int RIGHT  = 50;
+const int BOTTOM = 20;
+
+struct Point {
+    int x, y;
 };
-class CONRAN{
+
+class CONRAN {
 public:
-    struct Point A[100];
-    int DoDai;
-    CONRAN(){
+    Point A[100];   // thân rắn
+    int DoDai;      // độ dài rắn
+
+    CONRAN() {
         DoDai = 3;
         A[0].x = 10; A[0].y = 10;
         A[1].x = 11; A[1].y = 10;
         A[2].x = 12; A[2].y = 10;
     }
-    void Ve(){
-        for (int i = 0; i < DoDai; i++){
-            gotoxy(A[i].x,A[i].y);
-            cout<<"X";
+
+    void Ve() {
+        for (int i = 0; i < DoDai; i++) {
+            gotoxy(A[i].x, A[i].y);
+            cout << "X";
         }
     }
-    void DiChuyen(int Huong){
-        for (int i = DoDai-1; i>0;i--)
-            A[i] = A[i-1];
-        if (Huong==0) A[0].x = A[0].x + 1;
-        if (Huong==1) A[0].y = A[0].y + 1;
-        if (Huong==2) A[0].x = A[0].x - 1;
-        if (Huong==3) A[0].y = A[0].y - 1;
 
+    // Huong: 0 = phải, 1 = xuống, 2 = trái, 3 = lên
+    void DiChuyen(int Huong) {
+        for (int i = DoDai - 1; i > 0; i--) {
+            A[i] = A[i - 1];
+        }
+
+        if (Huong == 0) A[0].x = A[0].x + 1; // phải
+        if (Huong == 1) A[0].y = A[0].y + 1; // xuống
+        if (Huong == 2) A[0].x = A[0].x - 1; // trái
+        if (Huong == 3) A[0].y = A[0].y - 1; // lên
     }
 };
-
-int main()
-{
-    CONRAN r;
-    int Huong = 0;
-    char t;
-
-    while (1){
-        if (kbhit()){
-            t = getch();
-            if (t=='a') Huong = 2;
-            if (t=='w') Huong = 3;
-            if (t=='d') Huong = 0;
-            if (t=='x') Huong = 1;
-        }
-        system("cls");
-        r.Ve();
-        r.DiChuyen(Huong);
-        Sleep(300);
-    }
-
-    return 0;
-}
-
-
-void gotoxy( int column, int line )
-  {
-  COORD coord;
-  coord.X = column;
-  coord.Y = line;
-  SetConsoleCursorPosition(
-    GetStdHandle( STD_OUTPUT_HANDLE ),
-    coord
-    );
-  }
